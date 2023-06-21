c2a5ad36652908e4e10d08cc53c91b97dc72bf80
Fernando Aguilar <sparkmanperspective@gmail.com>
2023-06-21 17:37:54 -0400

    Updated Square  component to "remember" the STATE of it. We use useState function to do this.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
 // This is the child component. Prop passed from Board().
+// By calling setValue from an onClick handler, we tell React to re-render that Square whenever its <button> is clicked. After the update, the Square’s value will be 'X', so we’ll see the X on the game board. If you click on any Square, an X should show up.
 function Square(props) {
+  const [value, setValue] = useState('');
+
   return (
-    <button className="square">
-      {props.value}
-    </button>
+    <button
+      className="square"      
+      
+      onClick={() => setValue('X')}    >
+      {value}    </button>
   );
 }
 
 // This is the parent component.
 function Board() {
+  
   function renderSquare(i) {
     return <Square value={i} />;
   }
